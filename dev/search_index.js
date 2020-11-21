e2266bb0e9813fe449c5cedabf8253862943ff9e
Documenter.jl <documenter@juliadocs.github.io>
2020-11-21 11:45:32 +0000

    build based on 166ce7d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RetroSignalModel","category":"page"},{"location":"#RetroSignalModel","page":"Home","title":"RetroSignalModel","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RetroSignalModel]","category":"page"}]
+}
