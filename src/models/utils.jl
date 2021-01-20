808578ab884bba3b47bbdf66b026bf697c49c05f
Steven Chiu <stevengogogo4321@gmail.com>
2021-01-20 17:18:27 +0800

    Catalyst update: remove reaction_func (#26)
    * Update CI with self-hosted machine (#25)
    
    * Change ci to self hosted (#24)
    
    * Update ci.yml
    
    * Remove reaction_func for new version of Catalyst
    
    * fix dataframe problem of boolean table

--- src/models/utils.jl
+++ src/models/utils.jl
@@ -7,10 +7,10 @@ Reference
 ---------
 1. Michaelis Menten. Wikipidia.
 =#
-@reaction_func MM(chem, k, kd)= k* chem / (kd + chem)
+MM(chem, k, kd)= k* chem / (kd + chem)
 
 #=
     HILL(chem, k, kd, n)
 Hill equation. 'k' is the activation coefficient; `kd` is the half-speed concentration, and `n` is Hill coefficient.
 =#
-@reaction_func HILL(chem, k, kd, n) = k* chem^n / (kd^n + chem^n)
+HILL(chem, k, kd, n) = k* chem^n / (kd^n + chem^n)
--- test/data.jl
+++ test/data.jl
@@ -1,7 +1,7 @@
 # Test the availity 
 
 
-RetroSignalModel.RTG_Response_Boolean["rtg1"]
+RetroSignalModel.RTG_Response_Boolean[!,"rtg1"]
 
-RetroSignalModel.RTG_Response_Boolean["rtg2"]
+RetroSignalModel.RTG_Response_Boolean[!,"rtg2"]
 
